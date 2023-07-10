96ef2dd7d0eb63a402b92df8a02d9816c22ae459
Andrew Kachnic <ajkachnic@protonmail.com>
2023-07-10 16:09:29 -0400

    fixes for mobile

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css';
+import Head from "next/head";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Head>
+        <meta name="viewport" content="width=device-width,inital-scale=1" />
+      </Head>
+    </>
+  );
 }
 
 export default MyApp;
