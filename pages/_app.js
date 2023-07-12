a97be7dca8dc2a36391eee1e567a8a364e867f0d
Claire Wang <clara32356@gmail.com>
2023-07-11 20:32:47 -0400

    revert to mobile fixes

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,7 @@
-import Head from "next/head";
-import "../styles/globals.css";
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Component {...pageProps} />
-      <Head>
-        <meta name="viewport" content="width=device-width,inital-scale=1" />
-      </Head>
-    </>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
